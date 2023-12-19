d337b524bba61338549049decec3e9e4963e2d19
pandas9 <81386636+pandas9@users.noreply.github.com>
2023-12-19 14:33:00 +0000

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+// run puppeteer on google cloud functions v2 node 18
+// min 1GB ram 0.5 vCPU
+// node 18
+// google cloud functions v2
---
+++ index.js
@@ -0,0 +1,21 @@
+const functions = require('@google-cloud/functions-framework');
+const puppeteer = require('puppeteer');
+
+// min 1GB ram 0.5 vCPU
+
+functions.http('helloHttp', async (req, res) => {
+    try {
+        const data = req.body.html;
+
+        const browser = await puppeteer.launch({ headless: 'new', args: ["--no-sandbox", "--disable-setuid-sandbox"] });
+        const page = await browser.newPage();
+
+        // do stuff
+
+        await browser.close();
+        res.status(200).send('Success');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(400).send(`Internal Server Error: ${error.message}`);
+    }
+});
